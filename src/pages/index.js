71bb3ec6da95e5ffa195756349e13da294d9377c
Dale Dale <dpalmerpdx@gmail.com>
2023-02-04 13:44:09 -0800

    birthday update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,113 +1,129 @@
-import * as React from "react"
-import "../styles/index.css"
-import frog from "../images/frog.png"
+import * as React from "react";
+import "../styles/index.css";
+import cabin from "../images/cabin.jpeg";
 
 // styles
 const pageStyles = {
   color: "#232129",
   padding: "1em",
-  backgroundColor: "mediumaquamarine",
-  fontFamily: "fantasy",
+  backgroundColor: "forestgreen",
   display: "flex",
   alignItems: "center",
-  justifyContent: "center"
-}
+  justifyContent: "center",
+  fontFamily: "Verdana",
+};
 const frogDiv = {
   color: "#232129",
   padding: "1em",
-  background: `url(${frog})`,
-  backgroundSize: "contain",
-  height: "120vh",
-  backgroundRepeat: "no-repeat",
-  fontFamily: "fantasy",
-  flex: .5,
+  flex: 0.5,
   display: "flex",
   alignItems: "center",
-  justifyContent: "center"
-}
+  justifyContent: "center",
+  flexDirection: "column",
+};
+const frogPic = {
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  margin: "0 0 20px 0",
+  borderRadius: "20px",
+};
 const headingStyles = {
   marginTop: 0,
   marginBottom: 32,
   padding: 20,
-  textAlign: "center"
-}
+  textAlign: "center",
+};
 const jumbotronStyles = {
-  backgroundColor: "rgb(28 200 239 / 53%)",
+  backgroundColor: "#1b4b14",
   backdropFilter: "blur(1px)",
   border: "purple 1px solid",
   padding: 20,
-  width: "66vw"
-}
+  color: "white",
+  width: "66vw",
+};
 const paragraphStyles = {
-  display: 'flex',
+  display: "flex",
   fontWeight: 900,
-  textShadow: "#868689 0px 0px 11px",
-  fontSize: 20
-}
+  fontSize: 20,
+};
 
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <div style={frogDiv}>
-      <title>Camping 2022 - Thirty Thrive</title>
-      <div style={jumbotronStyles}>
-      <h1 className="draw" style={headingStyles}>
-        Memorial Day Camping!
-        <br/>
-        <span className="spin" role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <div style={paragraphStyles}>
-        <div  style={{flex: 1}}>
-          <p style={{textAlign: "center"}}>Info:</p>
-          <ul>
-            <li>When: Friday May 27th - Monday May 30th</li>
-            <li>Drop in anyday!</li>
-            <li>
-              Where: Frog Lake, Mt Hood National Forest
-            </li>
-            <li>
-              <a href="https://fs.usda.gov/recarea/mthood/recarea/?recid=53102">Frog Lake - Forest Service</a>
-            </li>
-            <li>
-              <a href="https://www.recreation.gov/camping/campgrounds/232848">Frog Lake - Recreation</a>
-            </li>
-            <li>
-              What: Fun, Food and Friends
-            </li>
-          </ul>
+        <img style={frogPic} src={cabin} />
+        <title>Camping 2022 - Thirty Thrive</title>
+        <div style={jumbotronStyles}>
+          <h1 style={headingStyles}>
+            Birthday Cabin
+            <br />
+            <span className="spin" role="img" aria-label="Party popper emojis">
+              🎉🎉🎉
+            </span>
+          </h1>
+          <div style={paragraphStyles}>
+            <div style={{ flex: 1 }}>
+              <p style={{ textAlign: "center" }}>Info:</p>
+              <ul>
+                <li>When: Tuesday May 30th - Thursday June 1st</li>
+                <li>Drop in anyday!</li>
+                <li>Where: Government Mineral Springs Guard Station</li>
+                <li>
+                  <a href="https://www.fs.usda.gov/detail/r6/recreation/?cid=fsbdev2_026644">
+                    Govt Mineral Spring Guard Station Forest Service
+                  </a>
+                </li>
+                <li>
+                  <a href="https://www.recreation.gov/camping/campgrounds/234091">
+                    Govt Mineral Spring Guard Station - Recreation
+                  </a>
+                </li>
+                <li>What: Fun, Food and Friends</li>
+              </ul>
+            </div>
+            <div style={{ flex: 1 }}>
+              <p style={{ textAlign: "center" }}>Activities:</p>
+              <ul>
+                <li>
+                  Biking -&gt;
+                  <ul>
+                    <li>Tons of forest roads!</li>
+                  </ul>
+                </li>
+                <li>
+                  Hiking -&gt;
+                  <ul>
+                    <li>Trapper Creek Wilderness</li>
+                    <li>Falls Creek Falls</li>
+                    <li>Panther Creek Falls</li>
+                  </ul>
+                </li>
+                <li>Frisbee</li>
+                <li>Charades</li>
+                <li>
+                  Food -&gt; Thinking veggie forward and maybe some pizza. The
+                  cabin has a refrigerator and oven
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            There is a max of 9 at the cabin but there is a campground .5 mile
+            from the cabin we could use for overflow
+          </p>
+          <a href="https://www.fs.usda.gov/recarea/giffordpinchot/recarea/?recid=31772">
+            Govt Mineral Spring Guard Station Forest Service
+          </a>
+          <p>Feedback is welcome as well as appreciated!</p>
+          <p>
+            Please text me with design ideas for the site and general ideas for
+            the weekend.
+          </p>
         </div>
-        <div style={{flex: 1}}>
-          <p style={{textAlign: "center"}}>Activities:</p>
-          <ul>
-            <li>Biking -&gt; 
-              <a href="https://bikepacking.com/routes/frog-lake-loop/">Frog Lake Look</a>
-            </li>
-            <li>Hiking -&gt; 
-              <a href="https://www.alltrails.com/explore/trail/us/oregon/frog-lake-butte-road">
-                Frog Buttes
-              </a> 
-            </li>
-            <li>Fishing -&gt; Stocked early in the season with rainbow trout</li>
-            <li>Maybe Boats</li>
-            <li>Frisbee</li>
-            <li>Charades</li>
-            <li>Food -&gt; Thinking veggie forward and maybe some bonfire pizza</li>
-          </ul>
-        </div>
-      </div>
-      <p>
-        Feedback is welcome as well as appreciated!
-      </p>
-      <p>
-        Please text me with design ideas for the site and general ideas for the weekend.
-      </p>
-      </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
