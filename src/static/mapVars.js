d13076e32796e9d53767dc6834e9f8e1382abcd3
Dale Dale <dpalmerpdx@gmail.com>
2023-02-04 13:53:05 -0800

    removes static folder

--- src/static/mapVars.js
+++
@@ -1,7 +0,0 @@
-var focusForest = "110603";
-var areaExtent = "-121.99566,45.88209,-121.99566,45.88209";
-var recLevel = 4;
-var contextPath = "https://www.fs.usda.gov/wps/PA_FSMap";
-var homeUrl = "https://www.fs.usda.gov/wps/PA_FSMap";
-var markerServletHome = "https://www.fs.usda.gov/nfs/portalsvc";
-var hostUrl = "";
